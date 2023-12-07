2daed761e0739edd61977b7e6baa4ba605640c19
Adith Gunaseelan <adithnishanth@gmail.com>
2023-12-07 18:50:07 -0500

    Sign-In page implemented

---
+++ constants/index.ts
@@ -0,0 +1,86 @@
+// NAVIGATION
+export const NAV_LINKS = [
+    { href: '/', key: 'home', label: 'Home' },
+    { href: '/', key: 'how_hilink_work', label: 'ChatBot' },
+    { href: '/', key: 'services', label: 'Services' },
+    { href: '/', key: 'pricing ', label: 'Pricing ' },
+    { href: '/', key: 'contact_us', label: 'Contact Us' },
+  ];
+  
+  // CAMP SECTION
+  export const PEOPLE_URL = [
+    '/person-1.png',
+    '/person-2.png',
+    '/person-3.png',
+    '/person-4.png',
+  ];
+  
+  // FEATURES SECTION
+  export const FEATURES = [
+    {
+      title: 'Real maps',
+      icon: '/map.svg',
+      variant: 'green',
+      description:
+        'Our chatbot uses real time updated maps to give you the best tourist attractions within a close proximity',
+    },
+    {
+      title: 'Assited Scheduling',
+      icon: '/calendar.svg',
+      variant: 'green',
+      description:
+        "TravelTrek helps with scheduling your trip with the given time period and amount of attractions you want to hit up",
+    },
+    {
+      title: 'Technology using Artificial Intelligence Capability',
+      icon: '/tech.svg',
+      variant: 'green',
+      description:
+        'Our AI chatbot is meant to feel like talking to a real travel agent helpingyou plan your next trip',
+    },
+    {
+      title: 'Many new locations every month',
+      icon: '/location.svg',
+      variant: 'orange',
+      description:
+        'We are alwayd updating our data to include new and upcoming spots in the destination area providing you wiht fine tuined results',
+    },
+  ];
+  
+  // FOOTER SECTION
+  export const FOOTER_LINKS = [
+    {
+      title: 'Learn More',
+      links: [
+        'About Hilink',
+        'Press Releases',
+        'Environment',
+        'Jobs',
+        'Privacy Policy',
+        'Contact Us',
+      ],
+    },
+    {
+      title: 'Our Community',
+      links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    },
+  ];
+  
+  export const FOOTER_CONTACT_INFO = {
+    title: 'Contact Us',
+    links: [
+      { label: 'Admin Officer', value: '123-456-7890' },
+      { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    ],
+  };
+  
+  export const SOCIALS = {
+    title: 'Social',
+    links: [
+      '/facebook.svg',
+      '/instagram.svg',
+      '/twitter.svg',
+      '/youtube.svg',
+      '/wordpress.svg',
+    ],
+  };
